fb62352a8cd14044ed6cc6a33dedcee95b5df512
Kamal <kamal18sharma@hotmail.com>
2020-11-05 03:39:22 +0530

    Commited by Kamal

--- Hello.py
+++ Hello.py
@@ -1,5 +1,6 @@
 print("Hello World")
-print ("Welcome to Python")
-print ("The world of new era Programming")
-num=555
+print("Welcome to Python")
+print("The world of new era Programming")
+# Following is example of number
+num = 555
 print(num)
